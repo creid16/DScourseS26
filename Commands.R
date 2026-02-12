e8cbfcb38691b6c929288ad2a0f7df8e66f053e5
creid16 <calebsreid@icloud.com>
2026-02-12 14:32:39 -0600

    Update Commands.R
    Rvest package

--- Commands.R
+++ Commands.R
@@ -27,7 +27,7 @@ counted.dplyr <- df %>% filter(Species=="virginica") %>% count %>% print()
 identical(counted[[1]],counted.dplyr[[1]])
 
 # Purrr ####
-library(purrr)
+suppressMessages(library(purrr))
 
 mtcars  %>%  
   split(mtcars$cyl) %>%  # from base R
@@ -39,3 +39,39 @@ mtcars %>%
   group_by(cyl) %>%
   summarize(r_squared=summary(lm(mpg~wt,data=cur_data()))$r.squared,
     .groups = "drop")
+
+# Rvest ####
+suppressMessages(library(rvest))
+suppressMessages(library(dplyr))
+
+url <- "http://en.wikipedia.org/wiki/Men%27s_100_metres_world_record_progression"
+
+webpage <- read_html(url) 
+
+olympicTablePre <- webpage %>%
+  html_element("#mw-content-text > div.mw-content-ltr.mw-parser-output > table:nth-child(11)") %>%
+  html_table(fill=T)
+
+olympicTable1976 <- webpage %>%
+  html_element("#mw-content-text > div.mw-content-ltr.mw-parser-output > table:nth-child(17)") %>%
+  html_table(fill=T)
+
+olympicTable1977 <- webpage %>% 
+  html_element("#mw-content-text > div.mw-content-ltr.mw-parser-output > table:nth-child(23)") %>% 
+  html_table(fill=T)
+
+olympicTable <- bind_rows(olympicTablePre,olympicTable1976,olympicTable1977)
+
+names(olympicTable)
+
+# Polite ####
+suppressMessages(library(polite))
+suppressMessages(library(janitor))
+
+
+session <- bow(url,force=T)
+
+result <- scrape(session,query=list(t="semi-soft",per_page=100)) %>% 
+  html_element("#mw-content-text > div.mw-content-ltr.mw-parser-output > table:nth-child(17)") %>%
+  html_table(fill=T)
+
