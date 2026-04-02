c734c827e0830623daa5f56a8c9297d856d6cf1b
creid16 <calebsreid@icloud.com>
2026-04-02 13:36:03 -0500

    Update Commands.R

--- Commands.R
+++ Commands.R
@@ -12,6 +12,7 @@
 # chmod +x *batch
 
 # scp "C:/Users/caleb/Downloads/PS6_Reid.R" "C:/Users/caleb/Downloads/PS6_Reid.tex" "C:/Users/caleb/Downloads/PS6_Reid.pdf" "C:/Users/caleb/Downloads/PS6a_Reid.png" "C:/Users/caleb/Downloads/PS6b_Reid.png" "C:/Users/caleb/Downloads/PS6c_Reid.png"  ouecon010@schooner.oscer.ou.edu:~/DScourseS26/ProblemSets/PS6/
+# scp "C:/Users/caleb/Downloads/PS7_Reid.R" "C:/Users/caleb/Downloads/PS7_Reid.tex" "C:/Users/caleb/Downloads/PS7_Reid.pdf" ouecon010@schooner.oscer.ou.edu:~/DScourseS26/ProblemSets/PS7/
 
 
 # SQLite ####
@@ -257,3 +258,74 @@ print(paste("The minimun of f(x) is ", gradient(x), sep=""))
 # 3. Algorithm
 # 4. Initial value
 # 5. Tolerance parameters
+
+# PS7 ####
+# Libraries
+library(mice)
+library(modelsummary)
+library(readr)
+library(dplyr)
+
+# Read in csv
+wages <- read_csv("ProblemSets/PS7/wages.csv")
+
+# Calculate missing data points for each feature
+naCount <- wages %>% 
+  summarize(across(everything(), ~sum(is.na(.))))
+
+# Remove rows where hgc or tenure is missing and update the df
+wages <- wages %>% 
+  filter(!is.na(hgc) & !is.na(tenure))
+
+# Count missing data points after removal
+naCount2 <- wages %>% 
+  summarize(across(everything(), ~sum(is.na(.))))
+
+# Summary table outputted to LaTeX
+table <- datasummary_skim(wages,"latex")
+
+# Listwise deletion of missing logwage
+modelData1 <- wages %>% filter(!is.na(logwage))
+
+# Listwise deletion model
+lm1 <- lm(logwage ~ hgc+college+tenure+tenure^2+age+married,modelData1)
+
+# Compute mean wages 
+meanLogwage <- mean(wages$logwage,na.rm=T)
+
+# Mean imputation of logwage
+modelData2 <- wages %>% 
+  mutate(logwage=if_else(is.na(logwage),meanLogwage,logwage))
+
+# Mean imputation model
+lm2 <- lm(logwage ~ hgc+college+tenure+tenure^2+age+married,modelData2)
+
+# Impute missing logwage with predicted logwage from listwise deletion model (lm1)
+modelData3 <- wages %>% 
+  mutate(predLogwage=predict(lm1,.),
+         logwage=if_else(is.na(logwage),predLogwage,logwage)) %>% 
+  select(-predLogwage)
+
+# Predicted values model
+lm3 <- lm(logwage ~ hgc+college+tenure+tenure^2+age+married,modelData3)
+
+# Data for mice model
+modelData4 <- wages
+
+# Impute with `mice`
+wagesMice <- mice(modelData4, m = 5, printFlag = FALSE)
+
+# Mice model
+lm4 <- with(wagesMice, lm(logwage ~ hgc+college+tenure+tenure^2+age+married)) 
+
+# Create empty list to store models
+models <- list()
+
+# Put models inside list
+models[['Listwise Deletion']] <- lm1
+models[['Mean Imputation']] <- lm2
+models[['Predicted Values']] <- lm3
+models[['Mice']] <- mice::pool(lm4)
+
+# Create summary table of the four models
+modelsummary(models)
